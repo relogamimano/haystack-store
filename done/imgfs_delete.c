7f22d4947202047a5ae53930ee4746cf87573a54
Matteo Pinto <mapinto@ic-24MA-spr-027.intranet.epfl.ch>
2024-04-29 18:11:36 +0200

    add delete

---
+++ done/imgfs_delete.c
@@ -0,0 +1,32 @@
+#include "imgfs.h"
+#include "error.h"
+#include <string.h>
+
+int do_delete(const char* img_id, struct imgfs_file* imgfs_file) {
+    if (imgfs_file == NULL || img_id == NULL) {
+        return ERR_INVALID_ARGUMENT;  
+    }
+
+    int found = 0;
+    for (unsigned int i = 0; i < imgfs_file->header.max_files; i++) {
+        if (strcmp(imgfs_file->metadata[i].img_id, img_id) == 0 && imgfs_file->metadata[i].is_valid == NON_EMPTY) {
+            imgfs_file->metadata[i].is_valid = EMPTY;  
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        return ERR_IMAGE_NOT_FOUND; 
+    }
+
+    imgfs_file->header.version++; 
+    imgfs_file->header.nb_files--;  
+
+    fseek(imgfs_file->file, sizeof(struct imgfs_header), SEEK_SET);
+    fwrite(imgfs_file->metadata, sizeof(struct img_metadata), imgfs_file->header.max_files, imgfs_file->file);
+    rewind(imgfs_file->file);
+    fwrite(&(imgfs_file->header), sizeof(struct imgfs_header), 1, imgfs_file->file);
+
+    return ERR_NONE;
+}
