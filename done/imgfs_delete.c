85817d3359d2c5c70380722ac107e5fb2516a7cf
Matteo Pinto <mapinto@ic-24MA-spr-021.intranet.epfl.ch>
2024-05-02 18:57:25 +0200

    delete tkt

--- done/imgfs_delete.c
+++ done/imgfs_delete.c
@@ -4,11 +4,8 @@
 
 int do_delete(const char* img_id, struct imgfs_file* imgfs_file) {
 
-    // utiliser m_require non null a la place 
-    if (imgfs_file == NULL || img_id == NULL) {
-        return ERR_INVALID_ARGUMENT;  
-    }
-    // 
+    M_REQUIRE_NON_NULL(imgfs_file); 
+    M_REQUIRE_NON_NULL(img_id); 
 
     if (imgfs_file->file == NULL) { puts("debug trace imgfs_file-> file is NULL");}
 
@@ -28,18 +25,23 @@ int do_delete(const char* img_id, struct imgfs_file* imgfs_file) {
     imgfs_file->header.version++; 
     imgfs_file->header.nb_files--;  
 
-    // 
+    // faire peutetre des ERR_IO et inverser version++ et files-- si ces 4 b ails fonctionnent pas 
     fseek(imgfs_file->file, sizeof(struct imgfs_header), SEEK_SET);
-    fwrite(imgfs_file->metadata, sizeof(struct img_metadata), imgfs_file->header.max_files, imgfs_file->file);
-    rewind(imgfs_file->file);
-    fwrite(&(imgfs_file->header), sizeof(struct imgfs_header), 1, imgfs_file->file);
+    fwrite(imgfs_file->metadata, sizeof(struct img_metadata), imgfs_file->header.max_files, imgfs_file->file); // on est d'accord 3)
+    // non rewind(imgfs_file->file);
+    fwrite(&(imgfs_file->header), sizeof(struct imgfs_header), 1, imgfs_file->file); // ça on est d'accord 2)
     // 
+
+
+    fseek(imgfs_file->file, 0, SEEK_SET); 
+
 
     if (fseek(imgfs_file->file, 0, SEEK_SET)) {
         imgfs_file->header.version--, 
         imgfs_file->header.nb_files++; 
         return ERR_IO; 
     }
+
 
     return ERR_NONE;
 }
