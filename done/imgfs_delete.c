b999f331ee725c17c4ac67700545c384a37d6774
Matteo Pinto <mapinto@ic-24MA-spr-019.intranet.epfl.ch>
2024-05-05 19:47:49 +0200

    dedup delete fix

--- done/imgfs_delete.c
+++ done/imgfs_delete.c
@@ -2,13 +2,12 @@
 #include "error.h"
 #include <string.h>
 
-int do_delete(const char* img_id, struct imgfs_file* imgfs_file) { //m 
+int do_delete(const char* img_id, struct imgfs_file* imgfs_file) {
 
-    // utiliser m_require non null a la place 
-    if (imgfs_file == NULL || img_id == NULL) {
-        return ERR_INVALID_ARGUMENT;  
-    }
-    // 
+    //puts("yo"); 
+
+    M_REQUIRE_NON_NULL(imgfs_file); 
+    M_REQUIRE_NON_NULL(img_id); 
 
     if (imgfs_file->file == NULL) { puts("debug trace imgfs_file-> file is NULL");}
 
@@ -28,15 +27,33 @@ int do_delete(const char* img_id, struct imgfs_file* imgfs_file) { //m
     imgfs_file->header.version++; 
     imgfs_file->header.nb_files--;  
 
-    // 
-    fseek(imgfs_file->file, sizeof(struct imgfs_header), SEEK_SET);
-    fwrite(imgfs_file->metadata, sizeof(struct img_metadata), imgfs_file->header.max_files, imgfs_file->file);
-    rewind(imgfs_file->file);
-    fwrite(&(imgfs_file->header), sizeof(struct imgfs_header), 1, imgfs_file->file);
+    // faire peutetre des ERR_IO et inverser version++ et files-- si ces 4 b ails fonctionnent pas 
+    //fseek(imgfs_file->file, sizeof(struct imgfs_header), SEEK_SET);
+    //fwrite(imgfs_file->metadata, sizeof(struct img_metadata), imgfs_file->header.max_files, imgfs_file->file); // on est d'accord 3)
+    // non rewind(imgfs_file->file);
+    //fwrite(&(imgfs_file->header), sizeof(struct imgfs_header), 1, imgfs_file->file); // ça on est d'accord 2)
     // 
 
+
+    //fseek(imgfs_file->file, 0, SEEK_SET); 
+
+
     if (fseek(imgfs_file->file, 0, SEEK_SET)) {
-        imgfs_file->header.version--, 
+        imgfs_file->header.version--; 
+        imgfs_file->header.nb_files++; 
+        puts("seek"); 
+        return ERR_IO; 
+    }
+
+    if (fwrite(&(imgfs_file->header), sizeof(struct imgfs_header), 1, imgfs_file->file) != 1) {
+        imgfs_file->header.version--; 
+        imgfs_file->header.nb_files++; 
+        return ERR_IO; 
+    }
+
+    if (fwrite(imgfs_file->metadata, sizeof(struct img_metadata), imgfs_file->header.max_files, imgfs_file->file) != imgfs_file->header.max_files) {
+        puts("write meta"); 
+        imgfs_file->header.version--;
         imgfs_file->header.nb_files++; 
         return ERR_IO; 
     }
