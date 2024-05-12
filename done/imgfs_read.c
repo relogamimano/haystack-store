6833f87bf1a36de9f2b6a878675a148cecdabb21
relogamimano <romeo.maignal@epfl.ch>
2024-05-12 18:14:46 +0200

    do_read() template returning default value just for testing purpose. To be updated by mateo

--- done/imgfs_read.c
+++ done/imgfs_read.c
@@ -4,7 +4,6 @@
 #include <stdio.h>
 
 
-int do_read(const char* img_id, int resolution, char** image_buffer,
-            uint32_t* image_size, struct imgfs_file* imgfs_file) {
-                return ERR_NONE;
-            }
+int do_read(const char* img_id, int resolution, char** image_buffer, uint32_t* image_size, struct imgfs_file* imgfs_file) {
+    return ERR_NONE;
+}
